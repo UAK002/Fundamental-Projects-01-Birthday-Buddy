e5686ec5ab0f16f682f340e465f42a9a287629ae
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-06-27 01:33:15 +0530

    169. Birthday Buddy - Import List

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
+import data from './data';
+
 const App = () => {
+  const [people, setPeople] = useState(data);
+  console.log(people);
   return <h2>Birthday Reminder - Starter</h2>;
-  console.log('test');
+  // console.log('test');
 };
 export default App;
