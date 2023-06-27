78cb7035de1bf6fa2817b2a39726bc8b68fedafc
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-06-27 10:59:20 +0530

    171. Birthday Buddy - Clear List

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ const App = () => {
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
+        <button
+          className="btn btn-block"
+          type="button"
+          onClick={() => setPeople([])}
+        >
+          clear all
+        </button>
       </section>
     </main>
   );
